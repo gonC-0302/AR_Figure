ac6334989477257ff006a0e30ffcad91c89f55af
gonC-0302 <gonc0302@gmail.com>
2024-06-06 21:56:43 +0900

    初回コミット

---
+++ Assets/@Sabo/Scripts/FigureTapAction.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FigureTapAction : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _rightFire, _leftFire;
+
+    private void Start()
+    {
+        SetUpEventTrigger();
+    }
+
+    private void SetUpEventTrigger()
+    {
+        EventTrigger trigger = gameObject.GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerClick;
+        entry.callback.AddListener((eventDate) => { OnTapFigure(); });
+        trigger.triggers.Add(entry);
+    }
+
+    private void OnTapFigure()
+    {
+        _rightFire.Play();
+        _leftFire.Play();
+    }
+}
